3a8ece0f68d9ccf67c7ab6106cfe21b39c27636d
Analia Alvarez <alvarezanaliar@gmail.com>
2022-07-12 21:00:33 -0300

    fix

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-// Preguntas a evaluar
-let preguntas = []
-let resultados = []
+let preguntas = null
+let resultados = null
+let preguntasSinMostrar = null
 let preguntaActual = 0
 
 function mostrarSiguientePregunta() {
+    
+    preguntaActual = getRandomQuestionIndex(preguntasSinMostrar)        
 
-    preguntaActual++
-    localStorage.setItem("preguntaActual", preguntaActual)
-
-    if (preguntaActual >= preguntas.length) {
+    if (preguntaActual === null) {
         mostrarResultados()
     } else {        
+        localStorage.setItem("preguntaActual", preguntaActual)            
         mostrarPregunta(preguntaActual)
     }    
+}
+
+function getRandomQuestionIndex(preguntas) {
+
+    if (preguntasSinMostrar.length == 0) {
+        return null
+    }
+
+    const indice = Math.floor(Math.random() * preguntasSinMostrar.length)
+    const preguntaIndice = preguntasSinMostrar[indice]
+    preguntasSinMostrar.splice(indice, 1)
+
+    localStorage.setItem("preguntasSinMostrar", JSON.stringify(preguntasSinMostrar))       
+
+    return preguntaIndice
 }
 
 function crearRespuesta(respuesta, i) {
@@ -66,7 +81,6 @@ function seleccionarResultado(respuesta) {
     })
 
     resultados.push(valorRespuesta)
-
     localStorage.setItem("resultados", JSON.stringify(resultados))
 
     mostrarSiguientePregunta()
@@ -88,13 +102,8 @@ function mostrarResultados() {
 
 function reset() {
 
-    resultados = []
-    localStorage.setItem("resultados", JSON.stringify(resultados))
-
-    preguntaActual = 0
-    localStorage.setItem("preguntaActual", preguntaActual)
-
-    mostrarPregunta(0)
+    localStorage.clear()
+    init(preguntas)
 
     const container = document.getElementById("question-container")
     container.style = "display: block"
@@ -105,26 +114,36 @@ function reset() {
 
 function init(_preguntas) {
 
-    preguntas = _preguntas
-    preguntaActual = localStorage.getItem("preguntaActual") || 0    
+    preguntas = _preguntas    
+
+    try {
+        preguntasSinMostrar = JSON.parse(localStorage.getItem("preguntasSinMostrar"))                
+    } catch (e) {        
+    }    
+
+    if (!preguntasSinMostrar) {
+        preguntasSinMostrar = _preguntas.map((pregunta, indice) => indice)        
+    }    
+    
+    preguntaActual = localStorage.getItem("preguntaActual")            
+    if (!preguntaActual) {        
+        preguntaActual = getRandomQuestionIndex()    
+        localStorage.setItem("preguntaActual", preguntaActual)            
+    }
 
     try {
         resultados = JSON.parse(localStorage.getItem("resultados"))
-    } catch (e) {
-        resultados = []
+    } catch (e) {        
     }    
     if (!resultados) {
         resultados = []
-    }
-
-    if (preguntaActual >= preguntas.length) {
+    }    
+    
+    if (resultados.length == preguntas.length) {
         mostrarResultados()
     } else {
         mostrarPregunta(preguntaActual)
     }
-    
-    console.log(parseInt(preguntaActual) + 1)
-    console.log(resultados)
 
     document.getElementById("btn-siguiente").onclick = () => {
 
@@ -139,10 +158,14 @@ function init(_preguntas) {
             cancelButtonText: 'No'
         }).then((result) => {
             if (result.isConfirmed) {
+
+                resultados.push(false)
+                localStorage.setItem("resultados", JSON.stringify(resultados))
+
                 mostrarSiguientePregunta()
             }
         })        
     } 
 }
 
-fetch('/data/preguntas.json').then(response => response.json()).then(data => init(data))
+fetch('./data/preguntas.json').then(response => response.json()).then(data => init(data))
